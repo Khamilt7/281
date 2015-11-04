f7db0c81c6e880266cf0023d14546bf1a3f2fb39
Khamilt7 <khamilt7@lion.lmu.edu>
2015-11-03 23:40:45 -0800

    Tests added and tostring broke

--- LinkedDeque.java
+++ LinkedDeque.java
@@ -47,17 +47,189 @@ public class LinkedDeque {
         return firstnode.toString("");
     }
     
+    private static int attempts = 0;
+    private static int successes = 0;
+	//method to assist in testing carried out in "main"
+	private static void displaySuccessIfTrue(boolean value) {
+        attempts++;
+        successes += value ? 1 : 0;
+
+        System.out.println(value ? "success" : "failure");
+    }
+
     public static void main ( String[] args ) {
+        LinkedDeque emptyDeck = new LinkedDeque();
         LinkedDeque testDeque = new LinkedDeque();
-        //testDeque.insertLeft("dog");
-        //testDeque.insertLeft("cat");
-        //testDeque.insertLeft("dogcat");
-        testDeque.insertRight("catdog");
-        System.out.println(testDeque.toString());
-        //testDeque.deleteRight();
-        System.out.println(testDeque.size());
-        System.out.println(testDeque.toString());
-        System.out.println(testDeque.right());
+        testDeque.insertLeft("dog");
+        testDeque.insertLeft("cat");
+        //testDeque.insertRight("dogcat");
+        //testDeque.insertRight("catdog");
+        System.out.println("Testing Constructor");
+        try {
+            displaySuccessIfTrue(emptyDeck.size == 0);         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        try {
+            displaySuccessIfTrue(testDeque.size == 2);         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }        
+        System.out.println("Testing insertLeft");
+        try {
+            displaySuccessIfTrue(testDeque.left() == "cat");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        } 
+        LinkedDeque threeDeck = new LinkedDeque();
+        threeDeck.insertLeft("cat");
+        threeDeck.insertRight("dogcat");
+        threeDeck.insertRight("catdog");
+        threeDeck.insertRight(17);
+        try {
+            displaySuccessIfTrue(threeDeck.left() == "cat");         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }       
+        LinkedDeque fourDeck = new LinkedDeque();
+        LinkedDeque fiveDeck = new LinkedDeque();
+        fourDeck.insertLeft("cat");
+        fourDeck.insertLeft("dog");
+        fourDeck.insertLeft("lizard");
+        fourDeck.insertLeft("snake");
+        fourDeck.insertLeft("giraffe");
+        fiveDeck.insertRight("donkey");
+        try {
+            displaySuccessIfTrue(fourDeck.left() == "giraffe");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }  
+        try {
+            displaySuccessIfTrue(fourDeck.toString().equals("[giraffe][snake][lizard][dog][cat]"));  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }           
+        System.out.println("Testing insertRight");
+        try {
+            displaySuccessIfTrue(fiveDeck.right() == "donkey");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }        
+        LinkedDeque sixDeck = new LinkedDeque();
+        sixDeck.insertRight("first");
+        sixDeck.insertRight("middle");
+        sixDeck.insertRight("last");
+        try {
+            displaySuccessIfTrue(sixDeck.right() == "last");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        } 
+        try {
+            displaySuccessIfTrue(sixDeck.toString().equals("[last][middle][first]"));  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }      
+        System.out.println("Testing deleteLeft");
+        testDeque.deleteLeft();
+        try {
+            displaySuccessIfTrue(testDeque.left() == "dog");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }     
+        fourDeck.deleteLeft();
+        try {
+            displaySuccessIfTrue(fourDeck.left() == "snake");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }  
+        try {
+            displaySuccessIfTrue(fourDeck.toString().equals("[snake][lizard][dog][cat]"));
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        } 
+        fourDeck.deleteLeft();
+        try {
+            displaySuccessIfTrue(fourDeck.left() == "lizard");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }   
+        System.out.println("Testing deleteRight");
+        threeDeck.deleteRight();
+        try {
+            displaySuccessIfTrue(threeDeck.right() == "catdog");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }         
+        threeDeck.deleteRight();
+        try {
+            displaySuccessIfTrue(threeDeck.right() == "dogcat");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        System.out.println("Testing left");
+        LinkedDeque leftDeck = new LinkedDeque();
+        leftDeck.insertLeft("cat");
+        leftDeck.insertLeft("kitten"); 
+        leftDeck.insertLeft("mothercat");        
+        try {
+            displaySuccessIfTrue(leftDeck.left() == "mothercat");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        } 
+        try {
+            displaySuccessIfTrue(testDeque.left() == "dog");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        } 
+        System.out.println("Testing Right");
+        LinkedDeque rightDeck = new LinkedDeque();
+        rightDeck.insertRight("cat");
+        rightDeck.insertRight("kitten");
+        rightDeck.insertRight("mothercat");
+        try {
+            displaySuccessIfTrue(rightDeck.right() == "mothercat");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }   
+        try {
+            displaySuccessIfTrue(threeDeck.right() == "dogcat");  
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }            
+        System.out.println("Testing Size");
+        try {
+            displaySuccessIfTrue(emptyDeck.size == 0);         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        try {
+            displaySuccessIfTrue(testDeque.size == 1);         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }        
+        try {
+            displaySuccessIfTrue(leftDeck.size == 3);         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }       
+        try {
+            displaySuccessIfTrue(rightDeck.size == 3);         
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }         
+        System.out.println("Testing toString");
+        try {
+            displaySuccessIfTrue(rightDeck.toString() == "[cat][kitten][mothercat]"); 
+                System.out.println(rightDeck.toString());
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }  
+        try {
+            displaySuccessIfTrue(leftDeck.toString().equals("[mothercat][kitten][cat]")); 
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }        
+            System.out.println(successes + "/" + attempts + " tests passed.");
     }
     class Node {
         Object information;
